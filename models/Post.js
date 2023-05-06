9691a3f82f3dae1d86af3d78d6078a445a03163a
bear-muna <bearmuna@gmail.com>
2023-05-06 13:03:42 -0700

    created post model

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes, NOW } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        time_created: {
+            type: DataTypes.DATA,
+            defaultValue: NOW,
+        },
+        user_id : {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'User',
+                key: 'id'
+            }
+        }
+    }, {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+    }
+);
+
+module.exports = Post;
