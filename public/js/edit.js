32212c61dd3b80c7d9727b89abb901721629c16d
bear-muna <bearmuna@gmail.com>
2023-05-09 21:55:57 -0700

    created a delete post route with frontend

--- public/js/edit.js
+++ public/js/edit.js
@@ -20,4 +20,27 @@ const updatePost = async (e) => {
     }
 }
 
+const deletePost = async (e) => {
+    e.preventDefault();
+
+    const pathName = window.location.pathname.split('');
+    const index = pathName[pathName.length - 1];
+
+    const response = await fetch('/api/posts', {
+        method: 'DELETE',
+        body: JSON.stringify({ index }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Unable to delete post');
+    }
+}
+
+
+
+document.querySelector('#dlt-btn').addEventListener('click', deletePost);
+
 document.querySelector('.edit-form').addEventListener('submit', updatePost);
